c53c7738c4d8d4848d0de8b21069d1c2e4700868
Hamed Hosseini <hamedhosseini@Senya-MacBook-Pro.local>
2024-12-03 07:00:15 +0300

    [BugFix] Fix Startup unintended animation

--- LeitnerBox/Leitners/EmptyLeitnerAnimation.swift
+++ LeitnerBox/Leitners/EmptyLeitnerAnimation.swift
@@ -9,15 +9,11 @@ import SwiftUI
 
 struct EmptyLeitnerAnimation: View {
     @EnvironmentObject var viewModel: LeitnerViewModel
-    @State var isAnimating: Bool = false
-    @State private var progress: CGFloat = 0
 
     var body: some View {
         if viewModel.leitners.count == 0 {
             ZStack {
-                Rectangle()
-                    .animatableGradient(from: [.purple, .green], toColor: [.yellow, .red], progress: progress)
-                    .opacity(0.8)
+                GradientAnimationView()
                 ZStack {
                     VStack {
                         Image(systemName: "tray")
@@ -37,16 +33,27 @@ struct EmptyLeitnerAnimation: View {
             }
             .frame(width: 256, height: 256)
             .cornerRadius(24)
-            .onAppear {
+            .onTapGesture {
+                viewModel.showEditOrAddLeitnerAlert.toggle()
+            }
+        }
+    }
+}
+
+private struct GradientAnimationView: View {
+    @State var isAnimating: Bool = false
+    @State private var progress: CGFloat = 0
+    
+    var body: some View {
+        Rectangle()
+            .animatableGradient(from: [.purple, .green], toColor: [.yellow, .red], progress: progress)
+            .opacity(0.8)
+            .task {
                 withAnimation(.easeOut(duration: 5).repeatForever(autoreverses: true)) {
                     isAnimating = true
                     progress = 1
                 }
             }
-            .onTapGesture {
-                viewModel.showEditOrAddLeitnerAlert.toggle()
-            }
-        }
     }
 }
 
